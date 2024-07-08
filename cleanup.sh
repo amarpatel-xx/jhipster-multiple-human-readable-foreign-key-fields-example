0f6205320d3f55f307add6d56ec40b3ed7d7fd0e
Amar Premsaran Patel <amarppatel1@gmail.com>
2024-07-07 21:02:58 -0400

    Getting the postgresql.yml files to be auto configured with the correct port so they do not conflict.

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,1 @@
-rm -fr .yo-rc.json blog docker-compose gateway kubernetes node_modules store package.json package-lock.json README.md blogdto storedto
+rm -fr last-used-port.json .yo-rc.json blog docker-compose gateway kubernetes node_modules store package.json package-lock.json README.md blogdto storedto
--- generate-code.sh
+++ generate-code.sh
@@ -1,3 +1,5 @@
+echo "Cleaning up previous code..."
 sh cleanup.sh
+echo "Generating code..."
 jhipster --blueprints multiple-human-readable-foreign-key-fields jdl non-reactive-mf.jdl --monorepository --workspaces --skip-jhipster-dependencies
-sh copy-files.sh custom-files/ .
+#sh copy-files.sh custom-files/ .
