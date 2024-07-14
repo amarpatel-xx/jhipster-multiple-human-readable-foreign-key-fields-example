c58ec7b7cb9e59f117ee30dc7d63da1229ad213c
Amar Premsaran Patel <amarppatel1@gmail.com>
2024-07-14 12:46:30 -0400

    Reorganized code.

--- blog/src/main/webapp/app/entities/blog/post/post.test-samples.ts
+++ blog/src/main/webapp/app/entities/blog/post/post.test-samples.ts
@@ -6,27 +6,27 @@ export const sampleWithRequiredData: IPost = {
   id: '4df47ec5-73fc-4a80-9731-52d10c903306',
   title: 'strengthen',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-07-09T13:23'),
+  date: dayjs('2024-07-14T07:17'),
 };
 
 export const sampleWithPartialData: IPost = {
   id: '249a046a-b68d-49d8-9bbf-f62edfa3d0c1',
   title: 'versus than who',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-07-09T09:37'),
+  date: dayjs('2024-07-14T03:31'),
 };
 
 export const sampleWithFullData: IPost = {
   id: 'e9c3d8b9-6ec5-426b-9a0b-96bd53c44a5a',
   title: 'woot',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-07-09T05:37'),
+  date: dayjs('2024-07-13T23:31'),
 };
 
 export const sampleWithNewData: NewPost = {
   title: 'unhappy smash',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-07-08T22:00'),
+  date: dayjs('2024-07-13T15:54'),
   id: null,
 };
 
