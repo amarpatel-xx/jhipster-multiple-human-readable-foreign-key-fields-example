1515cd8dcdd61b91b105e8d15c364aa586878076
Amar Patel <amarppatel1@gmail.com>
2024-09-14 08:35:35 -0400

    Do not cleanup last-used-port(s) file since previously saved ports are good to keep so that Docker containers do not have to be reinitialized.

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,1 @@
-rm -fr last-used-port.json .yo-rc.json blog docker-compose gateway kubernetes node_modules store package.json package-lock.json blogdto storedto
+rm -fr .yo-rc.json blog docker-compose gateway kubernetes node_modules store package.json package-lock.json blogdto storedto
