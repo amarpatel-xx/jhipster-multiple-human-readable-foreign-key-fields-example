cf4f469616f2a3a67c310b65692dfc5ffade086a
Amar Premsaran Patel <amarppatel1@gmail.com>
2024-10-07 09:12:19 -0400

    Proper folder and generator organization and upgrade to latest JHipster.

--- blog/src/main/webapp/app/entities/blog/post/post.test-samples.ts
+++ blog/src/main/webapp/app/entities/blog/post/post.test-samples.ts
@@ -6,27 +6,27 @@ export const sampleWithRequiredData: IPost = {
   id: '3c87121c-0361-4db6-854a-4a6ddbf6ef30',
   title: 'notwithstanding into infatuated',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-10-02T14:42'),
+  date: dayjs('2024-10-07T09:34'),
 };
 
 export const sampleWithPartialData: IPost = {
   id: '34aa4e3e-a96c-4e41-a3fa-c120a561405a',
   title: 'before dandelion',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-10-02T15:28'),
+  date: dayjs('2024-10-07T10:20'),
 };
 
 export const sampleWithFullData: IPost = {
   id: '9350336c-dc3c-4302-acdb-08bf53a97d8c',
   title: 'bob fooey dislocate',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-10-02T16:56'),
+  date: dayjs('2024-10-07T11:48'),
 };
 
 export const sampleWithNewData: NewPost = {
   title: 'purple irk mad',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-10-02T01:39'),
+  date: dayjs('2024-10-06T20:31'),
   id: null,
 };
 
