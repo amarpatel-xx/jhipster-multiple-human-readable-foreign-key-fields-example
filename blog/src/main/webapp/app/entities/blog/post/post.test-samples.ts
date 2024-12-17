c637affaba4fecc754edfe4d6398a998f35c8cd8
Amar P. Patel <amarppatel1@gmail.com>
2024-12-17 10:19:45 -0500

    Removed unecessary code.

--- blog/src/main/webapp/app/entities/blog/post/post.test-samples.ts
+++ blog/src/main/webapp/app/entities/blog/post/post.test-samples.ts
@@ -6,27 +6,27 @@ export const sampleWithRequiredData: IPost = {
   id: '3c87121c-0361-4db6-854a-4a6ddbf6ef30',
   title: 'notwithstanding into infatuated',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-11-25T17:15'),
+  date: dayjs('2024-12-17T11:28'),
 };
 
 export const sampleWithPartialData: IPost = {
   id: '34aa4e3e-a96c-4e41-a3fa-c120a561405a',
   title: 'before dandelion',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-11-25T18:01'),
+  date: dayjs('2024-12-17T12:14'),
 };
 
 export const sampleWithFullData: IPost = {
   id: '9350336c-dc3c-4302-acdb-08bf53a97d8c',
   title: 'bob fooey dislocate',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-11-25T19:29'),
+  date: dayjs('2024-12-17T13:42'),
 };
 
 export const sampleWithNewData: NewPost = {
   title: 'purple irk mad',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-11-25T04:12'),
+  date: dayjs('2024-12-16T22:25'),
   id: null,
 };
 
