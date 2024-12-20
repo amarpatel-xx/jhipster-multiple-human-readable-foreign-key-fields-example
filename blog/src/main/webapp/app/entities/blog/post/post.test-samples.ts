740545f3c4bd2d9282b0f13fb953ecdc3dd0da5c
Amar P. Patel <amarppatel1@gmail.com>
2024-12-20 12:46:07 -0500

    updated for latest JHipster.

--- blog/src/main/webapp/app/entities/blog/post/post.test-samples.ts
+++ blog/src/main/webapp/app/entities/blog/post/post.test-samples.ts
@@ -6,27 +6,27 @@ export const sampleWithRequiredData: IPost = {
   id: '3c87121c-0361-4db6-854a-4a6ddbf6ef30',
   title: 'notwithstanding into infatuated',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-12-17T11:28'),
+  date: dayjs('2024-12-20T13:58'),
 };
 
 export const sampleWithPartialData: IPost = {
   id: '34aa4e3e-a96c-4e41-a3fa-c120a561405a',
   title: 'before dandelion',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-12-17T12:14'),
+  date: dayjs('2024-12-20T14:44'),
 };
 
 export const sampleWithFullData: IPost = {
   id: '9350336c-dc3c-4302-acdb-08bf53a97d8c',
   title: 'bob fooey dislocate',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-12-17T13:42'),
+  date: dayjs('2024-12-20T16:12'),
 };
 
 export const sampleWithNewData: NewPost = {
   title: 'purple irk mad',
   content: '../fake-data/blob/hipster.txt',
-  date: dayjs('2024-12-16T22:25'),
+  date: dayjs('2024-12-20T00:55'),
   id: null,
 };
 
